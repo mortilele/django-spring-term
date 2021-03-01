48f2d8d89b293ade570d5ce3d00932545a6342d4
Alik <ali.mars.99@gmail.com>
2021-03-01 22:48:10 +0600

    LAB#4 uploaded, setup psycopg2, little bit customization admin models

--- todo/main/admin.py
+++ todo/main/admin.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 from main.models import ToDoGroup, ToDoItem
 
 
+@admin.register(ToDoItem)
+class ToDoItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'due_date', 'status', 'owner', 'group')
+    list_filter = ('status', 'group', 'owner')
+    list_editable = ('status', )
+    search_fields = ('owner__username', 'group__name')
+
+
 class ToDoItemInline(admin.TabularInline):
     model = ToDoItem
+    extra = 1
 
 
 @admin.register(ToDoGroup)
 class ToDoGroupModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+    search_fields = ('name', )
     inlines = [ToDoItemInline]
